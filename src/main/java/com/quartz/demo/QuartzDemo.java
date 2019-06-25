82111a9de072f19115c882b614c9ff9df05d78b0
皇甫嗷嗷叫 <45344257+huangfusuper@users.noreply.github.com>
2019-06-25 10:02:37 +0800

    Update QuartzDemo.java

--- src/main/java/com/quartz/demo/QuartzDemo.java
+++ src/main/java/com/quartz/demo/QuartzDemo.java
@@ -15,3 +15,22 @@ public class QuartzDemo implements Job {
         System.out.println("纯净版Quartz");
     }
 }
+
+
+//玩
+class test11{
+    public static void main(String[] args) {
+        JFrame f = new JFrame("测试标题");
+        f.setSize(400,600);
+        f.setLocation(500,50);
+        Button button = new Button("1");
+        Button button1 = new Button("2");
+        f.add(button);
+        f.add(button1);
+        f.setLayout(new FlowLayout());
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+
+
+}
